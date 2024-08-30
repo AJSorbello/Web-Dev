e2526a9451bf8c9d1dd1489050d28e0cb0843d43
AJ Sorbello <ajsorbello@gmail.com>
2024-08-30 13:11:15 -0700

    added protection login logout

--- book-store/src/books/views.py
+++ book-store/src/books/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render  # imported by default
 from django.views.generic import ListView, DetailView  # to display lists
 from .models import Book  # to access Book model
-
+#to protect class-based view
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class BookListView(ListView):  # class-based view
-    model = Book  # specify model
-    template_name = "books/main.html"  # specify template
+class BookListView(LoginRequiredMixin, ListView):     #class-based “protected” view
+    model = Book                                    #specify model
+    template_name = 'books/main.html'               #specify template
 
 
-class BookDetailView(DetailView):  # class-based view
-    model = Book  # specify model
-    template_name = "books/detail.html"  # specify template
+class BookDetailView(LoginRequiredMixin, DetailView):   #class-based “protected” view
+    model = Book                                    #specify model
+    template_name = 'books/detail.html'             #specify template
 
 
 # Create your views here.
 def home(request):
     return render(request, "books/home.html")
+
